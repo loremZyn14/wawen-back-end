c16d10489a7516f33d082fe358fc90df6d11ff64
LoremZyn14 <yasuaimo@gmail.com>
2020-10-26 13:13:31 +0800

    Update Product and Order Mock

--- database/mock/ProductMock.php
+++ database/mock/ProductMock.php
@@ -9,81 +9,39 @@ class ProductMock
         return array(
             array(
                 'category_id' => 4,
-                'product_name' => 'Potato Chips',
-                'price' => 20.00,
+                'product_name' => 'Ube Halaya Jar 100 grams',
+                'price' => 100.00,
                 'unit' => 'pcs',
                 'description' => 'Made in China'
             ), // 1
             array(
                 'category_id' => 4,
-                'product_name' => 'Mang Juan',
-                'price' => 40.00,
+                'product_name' => 'Ube Halaya Jar in Lunch Pack Medium Size',
+                'price' => 120.00,
                 'unit' => 'pcs',
                 'description' => 'Made with Philippines'
             ),// 2
             array(
                 'category_id' => 1,
-                'product_name' => 'Hopia',
-                'price' => 50.00,
+                'product_name' => 'Ube Halaya Birthday Cake',
+                'price' => 350.00,
                 'unit' => 'pack',
                 'description' => 'Made with Heart'
             ),// 3
             array(
                 'category_id' => 1,
-                'product_name' => 'Wafellow',
-                'price' => 13.00,
+                'product_name' => 'Ubechi 8pcs',
+                'price' => 250.00,
                 'unit' => 'pcs',
                 'description' => 'Made with Heart'
             ),// 4
             array(
                 'category_id' => 1,
-                'product_name' => 'Fudge Bar',
-                'price' => 7.00,
+                'product_name' => 'Ubechi 1 doz',
+                'price' => 300.00,
                 'unit' => 'pcs',
                 'description' => 'Made with Heart'
             ),// 5
-            array(
-                'category_id' => 1,
-                'product_name' => 'Cheese Cake',
-                'price' => 7.00,
-                'unit' => 'pcs',
-                'description' => 'Made with Heart'
-            ),// 6
-            array(
-                'category_id' => 2,
-                'product_name' => 'Pancit Canton',
-                'price' => 13.00,
-                'unit' => 'pcs',
-                'description' => 'Made with Heart'
-            ),// 7
-            array(
-                'category_id' => 2,
-                'product_name' => 'Lucky Me',
-                'price' => 10.00,
-                'unit' => 'pcs',
-                'description' => 'Made with Heart'
-            ),// 8
-            array(
-                'category_id' => 1,
-                'product_name' => 'Calcheese',
-                'price' => 13.00,
-                'unit' => 'pcs',
-                'description' => 'Made with Heart'
-            ),// 9
-            array(
-                'category_id' => 2,
-                'product_name' => 'Bulad',
-                'price' => 50.00,
-                'unit' => 'kilo',
-                'description' => 'Made with Heart'
-            ),// 10
-            array(
-                'category_id' => 3,
-                'product_name' => 'Rebisco Cracker',
-                'price' => 72.00,
-                'unit' => 'pack',
-                'description' => 'Made with Heart'
-            ),// 11
         );
     }
 }
